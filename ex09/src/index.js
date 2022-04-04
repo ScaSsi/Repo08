93b4cfa3ea319a11d9e258df01f6ebe3b7b3f159
Avujcic1 <avujcic1@etf.unsa.ba>
2022-04-04 16:15:08 +0200

    Functions - Random number generator

---
+++ ex09/src/index.js
@@ -0,0 +1,9 @@
+// Write code below this line
+function randomInteger() {
+    return Math.floor(Math.random() * 30);
+}
+
+randomInteger();
+// Write code above this line
+
+module.exports = randomInteger;
