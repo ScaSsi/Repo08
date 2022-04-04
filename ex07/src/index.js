8572c1550c5fcfafd11cecafa5cd5f3ab057c57f
Avujcic1 <avujcic1@etf.unsa.ba>
2022-04-04 16:07:57 +0200

    Functions - global vs local

---
+++ ex07/src/index.js
@@ -0,0 +1,12 @@
+// Write code below this line
+var dress = "little dress";
+
+function myDress() {
+    dress = "ball gown";
+    return dress;
+}
+
+myDress();
+// Write code above this line
+
+module.exports = myDress;
