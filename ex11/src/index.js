99d64ae281eeb5ad6ff4a4ea7a6419cedc31ba98
Avujcic1 <avujcic1@etf.unsa.ba>
2022-04-04 16:32:08 +0200

    Functions - radix

---
+++ ex11/src/index.js
@@ -0,0 +1,10 @@
+// Write code below this line
+function convertToInt(str) {
+    return console.log(parseInt(str, 16));
+}
+
+convertToInt("BA");
+convertToInt("F1");
+convertToInt("JeffBezos");
+// Write code above this line
+module.exports = convertToInt;
